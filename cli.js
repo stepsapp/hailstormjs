9b6a94cd582ee67a848fc08bd8a3b06fecea903a
Andreas Gruber <andreas.gruber@steps.app>
2022-09-29 10:42:33 +0200

    Make async

--- cli.js
+++ cli.js
@@ -7,38 +7,41 @@ var nodemon = require('nodemon')
 
 console.log(helpers.paths)
 
-var cmd = process.argv.slice(2)[0]
-switch (cmd) {
-    case 'start':
-        require('./lib/main')
-        break
-    case 'build':
-        require('./lib/build')
-        break
-    case 'init':
-        console.log('Copying files from module to application /src/pages')
-        await recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/pages`, `${helpers.paths.applicationRoot}/src/pages`)
-        console.log('Copying files from module to application /src/public')
-        await recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/public`, `${helpers.paths.applicationRoot}/src/public`)
-        console.log('Copying files from module to application /src/components')
-        await recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/components`, `${helpers.paths.applicationRoot}/src/components`)
-        break
-    case 'watch':
-        nodemon({
-            script: `${helpers.paths.moduleRoot}/lib/main.js`,
-            ext: 'js json css jsx html',
-            ignore: ['cache/**', 'dist/**'],
-        })
-        nodemon
-            .on('start', function () {
-                console.log('Building App.')
+const exec = async (cmd) => {
+    switch (cmd) {
+        case 'start':
+            require('./lib/main')
+            break
+        case 'build':
+            require('./lib/build')
+            break
+        case 'init':
+            console.log('Copying files from module to application /src/pages')
+            await recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/pages`, `${helpers.paths.applicationRoot}/src/pages`)
+            console.log('Copying files from module to application /src/public')
+            await recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/public`, `${helpers.paths.applicationRoot}/src/public`)
+            console.log('Copying files from module to application /src/components')
+            await recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/components`, `${helpers.paths.applicationRoot}/src/components`)
+            break
+        case 'watch':
+            nodemon({
+                script: `${helpers.paths.moduleRoot}/lib/main.js`,
+                ext: 'js json css jsx html',
+                ignore: ['cache/**', 'dist/**'],
             })
-            .on('quit', function () {
-                console.log('App has quit')
-                process.exit()
-            })
-            .on('restart', function (files) {
-                console.log('Re-Building App because of changes.')
-            })
-        break
+            nodemon
+                .on('start', function () {
+                    console.log('Building App.')
+                })
+                .on('quit', function () {
+                    console.log('App has quit')
+                    process.exit()
+                })
+                .on('restart', function (files) {
+                    console.log('Re-Building App because of changes.')
+                })
+            break
+    }
 }
+var cmd = process.argv.slice(2)[0]
+exec(cmd)
