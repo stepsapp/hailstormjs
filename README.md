2cb52bdde1bdbb4205ffbd16c8cea9992aad613e
Andreas Gruber <andreas.gruber@steps.app>
2022-09-29 10:21:40 +0200

    Fix paths

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# hailstormjs
+<p align="center">
+  <img width="150" height="150" src="./docs/images/hailstorm.png">
+</p>
+
+# Hailstormjs
 A real react static website generate
--- cli.js
+++ cli.js
@@ -1,35 +1,54 @@
-
 const path = require('path')
 const fs = require('fs')
-var babelConfig = path.join(__dirname, '/lib/config/babel.config.json')
-
-require('@babel/register')({
-    ignore: [],
-    configFile: babelConfig,
-})
-require('@babel/preset-env')
-require('@babel/preset-react')
-
-const appDirFullPath = path.dirname(require.main.filename)
-const appDirRegexResp = /^(.*?)node_modules/.exec(appDirFullPath)
-var appDir = appDirRegexResp ? appDirRegexResp[1] : appDirFullPath
-appDir = appDir.endsWith('/') ? appDir.slice(0, -1) : appDir
+const helpers = require('./lib/modules/helpers')
+require('./lib/modules/babel')
+const recursiveCopy = require('./lib/modules/recursiveCopyFiles')
+var nodemon = require('nodemon')
 
 var cmd = process.argv.slice(2)[0]
 switch (cmd) {
-    case 'init':
-        if (!fs.existsSync(`${appDir}/src`)) {
-            require(`${appDirFullPath}/lib/modules/recursiveCopyFiles`).copyFiles(`${appDirFullPath}/src`, `${appDir}/src`)
-        }
-        break;
     case 'start':
         require('./lib/main')
         break
     case 'build':
         require('./lib/build')
         break
+    case 'init':
+        if (!fs.existsSync(`${helpers.paths.applicationRoot}/src`)) {
+            console.log('Copying files from module to application root')
+            recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src`, `${helpers.paths.applicationRoot}/src`)
+        } else {
+            console.log('Application /src exists')
+        }
+        if (!fs.existsSync(`${helpers.paths.applicationRoot}/src/pages`)) {
+            console.log('Copying files from module to application /src/pages')
+            recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src/pages`, `${helpers.paths.applicationRoot}/src/pages`)
+        } else {
+            console.log('Application /src/pages exists')
+        }
+        if (!fs.existsSync(`${helpers.paths.applicationRoot}/src/public`)) {
+            console.log('Copying files from module to application /src/public')
+            recursiveCopy.copyFiles(`${helpers.paths.moduleRoot}/src`, `${helpers.paths.applicationRoot}/src/public`)
+        } else {
+            console.log('Application /src/public exists')
+        }
+        break
     case 'watch':
-        const watch = require('./lib/watcher')
-        watch()
+        nodemon({
+            script: `${helpers.paths.moduleRoot}/lib/main.js`,
+            ext: 'js json css jsx html',
+            ignore: ['cache/**', 'dist/**'],
+        })
+        nodemon
+            .on('start', function () {
+                console.log('Building App.')
+            })
+            .on('quit', function () {
+                console.log('App has quit')
+                process.exit()
+            })
+            .on('restart', function (files) {
+                console.log('Re-Building App because of changes.')
+            })
         break
 }
